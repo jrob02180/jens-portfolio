9671ceef6c625b85defda895f293689002f454dd
jrob02180 <jrob02180@gmail.com>
2023-05-15 14:24:21 -0600

    Changed all files in src folder to match activity 24

---
+++ src/components/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section id="aboutme">
+    <div class="title">
+      <h2>About Me</h2>
+    </div>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde explicabo
+      consequatur, nobis accusantium quod doloribus fuga! Eius similique
+      voluptatum possimus corrupti, voluptas mollitia ab cupiditate natus
+      ipsam aliquid facere ratione.
+      <br /><br />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+      perferendis minima quae mollitia amet placeat blanditiis ipsa adipisci
+      eum necessitatibus deserunt vero, sapiente provident facilis rerum porro
+      consectetur maxime aliquid?
+    </p>
+  </section>
+  );
+}
