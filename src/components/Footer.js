331059a9250504d0fa392e3be083988775f20d2c
jrob02180 <jrob02180@gmail.com>
2023-05-19 22:20:56 -0600

    Fixed footer layout, added insta image, replaced github image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import github from '../assets/images/github-mark-white.png';
-import linkedin from '../assets/images/linked-in.png';
+import React from "react";
+import github from "../assets/images/github-mark.png";
+import linkedin from "../assets/images/linked-in.png";
+import insta from "../assets/images/instagram.png";
 
 function Footer() {
-    return (
-        <footer className="footer">
-            <div className="container">
-              <a href='https://github.com/jrob02180'>
-                <img src={github} alt='github' />
-              </a>
-            </div>
-            <div className="container">
-              <a href='https://www.linkedin.com/in/jen-roberson-060417276/'>
-                <img src={linkedin} alt='linkedin' />
-              </a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="container d-flex justify-content-center align-items-center">
+        <a href="https://github.com/jrob02180">
+          <img src={github} alt="github" />
+        </a>
+        <a href="https://www.linkedin.com/in/jen-roberson-060417276/">
+          <img src={linkedin} alt="linkedin" />
+        </a>
+        <a href="https://www.instagram.com/">
+          <img src={insta} alt="instagram" />
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
