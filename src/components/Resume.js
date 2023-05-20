3eba45a8946d20307eabe129dc65e220563ceb3d
jrob02180 <jrob02180@gmail.com>
2023-05-19 23:23:00 -0600

    Adjusted layout on resume page. Just need to add resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,32 +1,35 @@
-import React from 'react';
+import React from "react";
 
 export default function Resume() {
   return (
     <div>
-      <p>Resume</p>
-      <p>
-     Download my resume
-      </p>
-      <p>
-        Front-end Proficiencies
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>jQuery</li>
-        <li>responsive design</li>
-        <li>React</li>
-        <li>Bootstrap</li>
-      </p>
-      <p>
-        Back-end Proficiencies
-      </p>
-      <li>APIs</li>
-      <li>Node</li>
-      <li>Express</li>
-      <li>MySQL, Sequelize</li>
-      <li>MongoDB, Mongoose</li>
-      <li>REST</li>
-      <li>GraphQL</li>
+      <div className="row download" style={{ fontSize: 50 }}>
+        <p>Download my resume</p>
+        <div className="row">
+          <div className="col-6" style={{ fontSize: 30 }}>
+            <p>
+              Front-end Proficiencies
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>jQuery</li>
+              <li>responsive design</li>
+              <li>React</li>
+              <li>Bootstrap</li>
+            </p>
+          </div>
+          <div className="col-6" style={{ fontSize: 30 }}>
+            <p>Back-end Proficiencies</p>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
