0b663bec8f78588f7c7890f25028bad38f0d899f
jrob02180 <jrob02180@gmail.com>
2023-05-21 21:19:33 -0600

    Centered layout on resume page, added border

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,33 +3,43 @@ import React from "react";
 export default function Resume() {
   return (
     <div>
-      <div className="row download" style={{ fontSize: 50 }}>
-        <p>Download my resume</p>
-        <div className="row">
-          <div className="col-6" style={{ fontSize: 30 }}>
-            <p>
-              Front-end Proficiencies
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>jQuery</li>
-              <li>responsive design</li>
-              <li>React</li>
-              <li>Bootstrap</li>
-            </p>
+      <div className="row resume text-center">
+        <p style={{ fontSize: 50 }}>Download my resume</p>
+        <div
+          className="col-6 d-flex justify-content-center skillset"
+          style={{ fontSize: 40, fontStyle: "italic" }}
+        >
+          Front-end Proficiencies
+        </div>
+        <div
+          className="col-6 d-flex justify-content-center skillset"
+          style={{ fontSize: 40, fontStyle: "italic" }}
+        >
+          Back-end Proficiencies
+        </div>
+          <div className="col-6 skillList"   style={{ fontSize: 30 }}>
+            <ul>HTML</ul>
+            <ul>HTML</ul>
+            <ul>CSS</ul>
+            <ul>JavaScript</ul>
+            <ul>jQuery</ul>
+            <ul>responsive design</ul>
+            <ul>React</ul>
+            <ul>Bootstrap</ul>
           </div>
-          <div className="col-6" style={{ fontSize: 30 }}>
-            <p>Back-end Proficiencies</p>
-            <li>APIs</li>
-            <li>Node</li>
-            <li>Express</li>
-            <li>MySQL, Sequelize</li>
-            <li>MongoDB, Mongoose</li>
-            <li>REST</li>
-            <li>GraphQL</li>
+          <div
+            className="col-6 skillList"
+            style={{ fontSize: 30 }}
+          >
+            <ul>APIs</ul>
+            <ul>Node</ul>
+            <ul>Express</ul>
+            <ul>MySQL, Sequeulze</ul>
+            <ul>MongoDB, Mongoose</ul>
+            <ul>REST</ul>
+            <ul>GraphQL</ul>
           </div>
         </div>
       </div>
-    </div>
   );
 }
